35cd8131c375375e62cdb0b3862378e2f98b0c77
Boriwat64 <119995755+Boriwat64@users.noreply.github.com>
2022-12-27 00:34:41 +0700

    Add files via upload

---
+++ Case-sensitive.js
@@ -0,0 +1,7 @@
+const status = 'error';
+
+if (status.toUpperCase()==='ERROR'){
+    console.log('Something wrong!');
+}else{
+    console.log('Looks great!!');
+}
---
+++ reverse2.js
@@ -0,0 +1,6 @@
+function reverseString(value){
+
+    const reversedValue = value.split('').revrse().join('')
+    return reversedValue;
+}
+console.log(reverseString("Hello JavaScript"));
