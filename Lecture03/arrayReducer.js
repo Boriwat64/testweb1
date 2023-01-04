742bcfd6d8d14adf5d392c983998f4ae86951465
Boriwat64 <6406021620131@fitm.kmutnb.ac.th>
2023-01-04 10:16:54 +0700

    udate arrayReducer.js

--- Lecture03/arrayReducer.js
+++ Lecture03/arrayReducer.js
@@ -1,11 +1,11 @@
 const array = [15,16,17,18,19];
 
 function reducer(accumulator,currentValue,index){
-    const retuns = accumulator + currentValue;
+    const returns = accumulator + currentValue;
     console.log(
         `accumulator: ${accumulator}, currentValue: ${currentValue}, index: ${index}, returns: ${returns}`,
     );
-    return retuns;
+    return returns;
 }
 
 array.reduce(reducer);
