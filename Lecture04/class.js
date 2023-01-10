61ef83d89c28be8727d6967b1c6e3a2740549f38
Boriwat64 <s6406021620131@email.kmutnb.ac.th>
2023-01-10 21:22:21 +0700

    update class.js

---
+++ Lecture04/class.js
@@ -0,0 +1,14 @@
+class Song {
+    constructor() {
+        this.title;
+        this.author;
+    }
+
+play() {
+    console.log("Song playing!");
+    }
+}
+const mySong = new Song();
+mySong.play();
+git config --global user.email "s6406021620131@email.kmutnb.ac.th"
+git config --global user.name "Boriwat64"
